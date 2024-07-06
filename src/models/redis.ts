688adce7fcb7d4b867a257bb694fe72ff0da4a81
Marcelo <mhbcoelho99@gmail.com>
2024-07-06 11:54:49 -0300

    server

--- src/models/redis.ts
+++
@@ -1,9 +0,0 @@
-import { Environment } from "./environment";
-
-export class Redis {
-  constructor(
-    public id: string,
-    public userId: string,
-    public environments: Environment[]
-  ) {}
-}
--- src/models/session.ts
+++
@@ -1,8 +0,0 @@
-export class Session {
-  constructor(
-    public id: string,
-    public userId: string,
-    public token: string,
-    public expiresAt: Date
-  ){}
-}
