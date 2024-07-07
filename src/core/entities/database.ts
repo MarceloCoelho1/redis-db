149aeec81b573a7867516e8def733ca1f1dbf123
Marcelo <mhbcoelho99@gmail.com>
2024-07-07 15:44:19 -0300

    feat: add attributes to the database entity

--- src/core/entities/database.ts
+++ src/core/entities/database.ts
@@ -3,8 +3,11 @@ export class Database {
     public id: string,
     public name: string,
     public host: string,
+    public username: string,
+    public isActive: boolean,
     public port: number,
     public password: string,
+    public secretKey: string,
     public environmentId: string
   ) {}
 }
