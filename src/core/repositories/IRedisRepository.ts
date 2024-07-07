ad5eec242a9acd929f3b1108ba37eeea24452a06
Marcelo <mhbcoelho99@gmail.com>
2024-07-07 15:45:29 -0300

    feat: create findEnvironmentById and createDatabase interfaces

--- src/core/repositories/IRedisRepository.ts
+++ src/core/repositories/IRedisRepository.ts
@@ -1,9 +1,13 @@
 import { CreateEnvironmentDTO } from "../../infra/http/dtos/createEnvironment";
+import { Database } from "../entities/database";
 import { Environment } from "../entities/environment";
 import { Redis } from "../entities/redis";
+import { databaseType } from "../types/database";
 
 export interface IRedisRepository {
   create(userId: string): Promise<Redis>;
   createEnvironment(data: CreateEnvironmentDTO): Promise<Environment>
   findRedisByUserId(userId: string): Promise<Redis | null>
+  createDatabase(data: databaseType): Promise<Database>
+  findEnvironmentById(id: string): Promise<Environment | null>
 }
